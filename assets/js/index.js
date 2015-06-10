810f1de407ccbfb3a6c35242a434ca8b7417d9e2
crackerhead <nbower@mac.com>
2015-06-09 22:59:31 -0700

    added bootstrap navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,13 +9,13 @@
     $(document).ready(function(){
 
         $(".post-content").fitVids();
-        
+
         // Calculates Reading Time
         $('.post-content').readingTime({
             readingTimeTarget: '.post-reading-time',
             wordCountTarget: '.post-word-count',
         });
-        
+
         // Creates Captions from Alt tags
         $(".post-content img").each(function() {
             // Let's put a caption if there is one
@@ -27,3 +27,20 @@
     });
 
 }(jQuery));
+
+//modification to the way bootstrap dropdowns work.
+//this makes them work on hover at normal rez and on click/touch on phone/tablet
+$(function () {
+  var $win = $(window);
+  $win.resize(function () {
+      if ($win.width() > 768)
+          $(".navbar-nav > .dropdown > a").attr("data-toggle", "");
+      else
+          $(".navbar-nav > .dropdown > a").attr("data-toggle", "dropdown");
+  }).resize();
+  $(".dropdown-menu").find("input, button").each(function () {
+      $(this).click(function (e) {
+          e.stopPropagation();
+      });
+  });
+});
